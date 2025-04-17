e9780c845a3212b7b45cc7ad165d6bd9245cb86d
Chase Fegenbush <chase.fegenbush22@gmail.com>
2025-04-16 20:43:19 -0500

    Tailwindcss setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono as GeistMono } from "next/font/google";
+import React from "react";
 import "./globals.css";
 
 const geistSans = Geist({
