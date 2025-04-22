d20b5dec7e81e9513ccb57638fbd894321a4fe21
Chase Fegenbush <chase.fegenbush22@gmail.com>
2025-04-22 13:11:45 -0500

    Mobile nav & Nav links

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,10 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
 
   console.log("Session", session);
-  return (
-    <div>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
