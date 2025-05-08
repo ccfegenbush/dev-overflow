0925aaa4ebba4ef2eec53e4bb9b2b0c93cd177f7
Chase Fegenbush <chase.fegenbush22@gmail.com>
2025-05-08 12:53:05 -0500

    API routes

--- lib/validations.ts
+++ lib/validations.ts
@@ -51,21 +51,21 @@ export const SignUpSchema = z.object({
 });
 
 export const AskQuestionSchema = z.object({
-  title: z.string().min(1, { message: "Title is required." }).max(100, {
-    message: "Title cannot exceed 100 characters.",
-  }),
+  title: z
+    .string()
+    .min(5, { message: "Title is required." })
+    .max(100, { message: "Title cannot exceed 100 characters." }),
+
   content: z.string().min(1, { message: "Body is required." }),
   tags: z
     .array(
       z
         .string()
-        .min(1, { message: "Tag is required" })
+        .min(1, { message: "Tag is required." })
         .max(30, { message: "Tag cannot exceed 30 characters." })
     )
-    .min(1, {
-      message: "At least one tag is required.",
-    })
-    .max(3, { message: "You can only add up to 3 tags." }),
+    .min(1, { message: "At least one tag is required." })
+    .max(3, { message: "Cannot add more than 3 tags." }),
 });
 
 export const UserSchema = z.object({
@@ -82,4 +82,29 @@ export const UserSchema = z.object({
     .url({ message: "Please provide a valid URL." })
     .optional(),
   reputation: z.number().optional(),
+});
+
+export const AccountSchema = z.object({
+  userId: z.string().min(1, { message: "User ID is required." }),
+  name: z.string().min(1, { message: "Name is required." }),
+  image: z.string().url({ message: "Please provide a valid URL." }).optional(),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long." })
+    .max(100, { message: "Password cannot exceed 100 characters." })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter.",
+    })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least one lowercase letter.",
+    })
+    .regex(/[0-9]/, { message: "Password must contain at least one number." })
+    .regex(/[^a-zA-Z0-9]/, {
+      message: "Password must contain at least one special character.",
+    })
+    .optional(),
+  provider: z.string().min(1, { message: "Provider is required." }),
+  providerAccountId: z
+    .string()
+    .min(1, { message: "Provider Account ID is required." }),
 });
