4fd29913cb28485d8ea45c9b8762c8ace51bd05c
Chase Fegenbush <chase.fegenbush22@gmail.com>
2025-05-08 13:09:06 -0500

    Implement fetch handler

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,18 +5,23 @@ import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import { api } from "@/lib/api";
+import handleError from "@/lib/handlers/error";
 
 const questions = [
   {
     _id: "1",
     title: "How to learn React?",
-    description: "I want to learn React, but I don't know where to start.",
-    tags: [{ _id: "1", name: "React" }],
+    description: "I want to learn React, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
     author: {
       _id: "1",
       name: "John Doe",
       image:
-        "https://static.vecteezy.com/system/resources/thumbnails/002/002/403/small/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
+        "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
     },
     upvotes: 10,
     answers: 5,
@@ -24,44 +29,52 @@ const questions = [
     createdAt: new Date(),
   },
   {
-    _id: "1",
+    _id: "2",
     title: "How to learn JavaScript?",
-    description: "I want to learn JavaScript, but I don't know where to start.",
-    tags: [{ _id: "1", name: "JavaScript" }],
+    description: "I want to learn JavaScript, can anyone help me?",
+    tags: [
+      { _id: "1", name: "JavaScript" },
+      { _id: "2", name: "JavaScript" },
+    ],
     author: {
       _id: "1",
       name: "John Doe",
       image:
-        "https://static.vecteezy.com/system/resources/thumbnails/002/002/403/small/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
+        "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
     },
     upvotes: 10,
     answers: 5,
     views: 100,
-    createdAt: new Date(),
+    createdAt: new Date("2021-09-01"),
   },
 ];
+
+const test = async () => {
+  try {
+    return await api.users.getAll();
+  } catch (error) {
+    return handleError(error);
+  }
+};
 
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
+  const users = await test();
+
+  console.log(users);
+
   const { query = "", filter = "" } = await searchParams;
 
   const filteredQuestions = questions.filter((question) => {
-    // Filter by search query
     const matchesQuery = question.title
       .toLowerCase()
-      .includes(query?.toLowerCase());
-
-    // Filter by technology tag if filter is present
+      .includes(query.toLowerCase());
     const matchesFilter = filter
-      ? question.tags.some(
-          (tag) => tag.name.toLowerCase() === filter.toLowerCase()
-        )
+      ? question.tags[0].name.toLowerCase() === filter.toLowerCase()
       : true;
-
-    // Return questions that match both conditions
     return matchesQuery && matchesFilter;
   });
 
@@ -69,11 +82,12 @@ const Home = async ({ searchParams }: SearchParams) => {
     <>
       <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
         <Button
           className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
           asChild
         >
-          <Link href={ROUTES.ASK_QUESTION}>Ask A Question</Link>
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
         </Button>
       </section>
       <section className="mt-11">
